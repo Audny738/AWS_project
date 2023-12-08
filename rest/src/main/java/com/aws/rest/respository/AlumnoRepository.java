9151d18464364462f89ba4905989fb5aadd74849
Desiree Correa <audny.correac@gmail.com>
2023-12-07 18:39:53 -0600

    modify repository

--- rest/src/main/java/com/aws/rest/respository/AlumnoRepository.java
+++ rest/src/main/java/com/aws/rest/respository/AlumnoRepository.java
@@ -2,17 +2,14 @@ package com.aws.rest.respository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.aws.rest.entity.Alumno;
 
 @Repository
-public class AlumnoRepository {
-    private List<Alumno> alumnosList = new ArrayList<>();
-
-    public List<Alumno> getAlumnosList() {
-        return alumnosList;
-    }
-
+public interface AlumnoRepository extends JpaRepository<Alumno, Integer> {
+    Optional<Alumno> findById(int alumnoId);
 }
--- rest/src/main/java/com/aws/rest/respository/ProfesorRepository.java
+++ rest/src/main/java/com/aws/rest/respository/ProfesorRepository.java
@@ -3,15 +3,13 @@ package com.aws.rest.respository;
 import com.aws.rest.entity.Profesor;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class ProfesorRepository {
-    private List<Profesor> profesoresList = new ArrayList<>();
-
-    public List<Profesor> getProfesoresList() {
-        return profesoresList;
-    }
+public interface ProfesorRepository extends JpaRepository<Profesor, Integer> {
+    Optional<Profesor> findById(int profesorId);
 
 }
